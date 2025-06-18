68325eb463c02e591611b12b84e4f233870fc882
Felipe Valboeno <feliperocha14@hotmail.com>
2025-06-18 07:48:14 -0300

    Primeiro commit - repositório criado

---
+++ exemplo_padrão_JS.js
@@ -0,0 +1,43 @@
+//Produção|Homologação
+/**
+ * arquivo: arquivox.js
+ * processo: [Teste] programação v.1
+ *
+ * - Dependências:
+ *
+ * Nota:
+ *
+ * @author      Felipe.valboeno
+ * @since       06/2025
+ */
+
+/* ******* *
+ * EVENTOS *
+ * ******* */
+
+/* ****** *
+ * RESETS *
+ * ****** */
+
+/* ************************* *
+ * CONFIGURACOES DE EXIBICAO *
+ * ************************* */
+
+/* *********** *
+ * TEXTOS HTML *
+ * *********** */
+
+/* ********** *
+ * VALIDAÇÕES *
+ * ********** */
+
+/* ********************* *
+ * CARREGAMENTO DE DADOS *
+ * ********************* */
+
+/* ****************** *
+ * FUNCOES AUXILIARES *
+ * ****************** */
+
+  
+  
